a38a1abc980506ddbf331fb88af7979d6a55fe87
Jed Simson <jed.simson@gmail.com>
2021-06-23 21:24:49 +1200

    Add missing XML documentation.

--- src/Reddit.NET.Client/Models/Internal/Json/CommentRepliesJsonConverter.cs
+++ src/Reddit.NET.Client/Models/Internal/Json/CommentRepliesJsonConverter.cs
@@ -5,26 +5,21 @@ using Reddit.NET.Client.Models.Internal.Base;
 
 namespace Reddit.NET.Client.Models.Internal.Json
 {
+    /// <summary>
+    /// A <see cref="JsonConverter{T}" /> implementation for reading JSON data to populate the <see cref="Internal.Comment.Details.Replies" /> property.
+    /// </summary>
     internal class CommentRepliesJsonConverter : JsonConverter<Listing<IHasParent>>
     {
-        public override Listing<IHasParent> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            if (reader.TokenType == JsonTokenType.StartObject)
+        /// <inheritdoc />        
+        public override Listing<IHasParent> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+            reader.TokenType switch
             {
-                // There are replies to the comment, so we can read a listing.
-                return JsonSerializer.Deserialize<Listing<IHasParent>>(ref reader, options);
-            }
+                JsonTokenType.StartObject => JsonSerializer.Deserialize<Listing<IHasParent>>(ref reader, options),
+                JsonTokenType.String when reader.GetString() == string.Empty => null,
+                _ => throw new JsonException("Unexpected JSON data for comment replies.")
+            };        
 
-            if (reader.TokenType == JsonTokenType.String && reader.GetString() == string.Empty)
-            {
-                // There are no replies to the comment, so just treat it as a null listing.
-                // TODO: Is it more appropriate to return an empty listing? 
-                return null;
-            }
-
-            throw new JsonException("Unexpected JSON data for comment replies.");
-        }
-
+        /// <inheritdoc />
         public override void Write(Utf8JsonWriter writer, Listing<IHasParent> value, JsonSerializerOptions options) =>
             throw new NotImplementedException();        
     }
