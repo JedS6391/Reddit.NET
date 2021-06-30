d16fcb7897bd3b1b41ed2b2b285a72f33f13483e
Jed Simson <jed.simson@gmail.com>
2021-07-01 10:54:41 +1200

    Add initial inbox interactor tests

---
+++ tests/Reddit.NET.Client.IntegrationTests/InboxInteractorTests.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Reddit.NET.Client.IntegrationTests.Shared;
+using Reddit.NET.Client.Models.Public.Listings.Options;
+
+namespace Reddit.NET.Client.IntegrationTests
+{
+    public class InboxInteractorTests
+    {
+        private RedditClient _client;
+
+        [SetUp]
+        public void Setup()
+        {
+            _client = TestRedditClientProvider.GetClient();
+        }
+
+        [Test]
+        public async Task GetDetailsAsync_ValidUser_ShouldGetDetails()
+        {
+            var inbox = _client.Me().Inbox();
+
+            var messages = await inbox
+                .GetMessagesAsync(builder =>
+                    builder
+                        .WithMessageType(InboxMessageType.All)
+                        .WithMaximumItems(10))
+                .ToListAsync();
+
+            Assert.IsNotNull(messages);
+            Assert.IsNotEmpty(messages);
+        }         
+    }
+}
