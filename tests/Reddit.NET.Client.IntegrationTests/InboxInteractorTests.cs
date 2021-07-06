e17a7416cd78ba4ad882443fba8d55dbab70529d
Jed Simson <jed.simson@gmail.com>
2021-07-06 19:02:17 +1200

    test: Add test for inbox message replies.

--- tests/Reddit.NET.Client.IntegrationTests/InboxInteractorTests.cs
+++ tests/Reddit.NET.Client.IntegrationTests/InboxInteractorTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -30,6 +31,30 @@ namespace Reddit.NET.Client.IntegrationTests
 
             Assert.IsNotNull(messages);
             Assert.IsNotEmpty(messages);
-        }         
+        } 
+
+        [Test]
+        public async Task ReplyAsync_ValidMessage_ShouldAddRepliesToMessage()
+        {
+            var inbox = _client.Me().Inbox();
+
+            var messages = await inbox
+                .GetMessagesAsync(builder =>
+                    builder
+                        .WithMessageType(InboxMessageType.All)
+                        .WithMaximumItems(10))
+                .ToListAsync();
+
+            Assert.IsNotNull(messages);
+            Assert.IsNotEmpty(messages);
+
+            var message = messages.First();
+            var replyText = $"Test reply made by Reddit.NET client integration tests [{Guid.NewGuid()}].";
+
+            var reply = await inbox.ReplyAsync(message, replyText);
+
+            Assert.IsNotNull(reply);
+            Assert.AreEqual(replyText, reply.Body);
+        }                
     }
 }
